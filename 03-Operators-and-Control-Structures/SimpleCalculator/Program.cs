cf4cdacad2fc1c7b99f7424a8315cb091c1e61a4
MennaCS2006 <menna2006fawzy@gmail.com>
2026-05-13 17:25:51 +0300

    Update

--- 03-Operators-and-Control-Structures/SimpleCalculator/Program.cs
+++ 03-Operators-and-Control-Structures/SimpleCalculator/Program.cs
@@ -3,11 +3,12 @@ class Program {
     static void Main(string[] args)
     {
         double result = 0;
+        bool ValidOp = true;
         Console.WriteLine("         =====Simple Calculator=====         ");
         Console.WriteLine("---------------------------------------------");
         Console.Write("Enter first number: ");
         double num1 = double.Parse(Console.ReadLine());
-        Console.WriteLine("Choose operation:\nA. +\nB. -\nC. *\nD. /\nE. %");
+        Console.WriteLine("Enter an operator (+, -, *, /, %) ");
         Console.Write("Your choice: ");
         char op = char.Parse(Console.ReadLine());
         Console.Write("Enter second number: ");
@@ -15,58 +16,78 @@ class Program {
         Console.WriteLine("---------------------------------------------");
         switch (op) 
         { 
-            case 'A':
-            case 'a':
+           
             case '+':
                 result = num1+num2;
-                Console.WriteLine($"{num1} + {num2} = {result}");
-                Console.WriteLine("---------------------------------------------");
                 break;
 
-            case 'B':
-            case 'b':
+            
             case '-':
                 result = num1 - num2;
-                Console.WriteLine($"{num1} - {num2} = {result}");
-                Console.WriteLine("---------------------------------------------");
                 break;
 
-            case 'C':
-            case 'c':
+            
             case '*':
                 result = num1 * num2;
-                Console.WriteLine($"{num1} * {num2} = {result}");
-                Console.WriteLine("---------------------------------------------");
                 break;
 
-            case 'D':
-            case 'd':
+            
             case '/':
                 if (num2 == 0) { 
                 Console.WriteLine("\nError: Cannot divide by zero!");
                 Console.WriteLine("---------------------------------------------");
+                    ValidOp = false;
                 }
 
                 else { 
                 result = num1 / num2;
-                Console.WriteLine($"{num1} / {num2} = {result}");
-                Console.WriteLine("---------------------------------------------");
-                }
+                     }
                 break;
 
-            case 'E':
-            case 'e':
+           
             case '%':
                 result = num1 % num2;
-                Console.WriteLine($"{num1} % {num2} = {result}");
-                Console.WriteLine("---------------------------------------------");
-                break;
+                 break;
 
             default:
+                ValidOp = false;
                 Console.WriteLine("Error: Invalid operator!");
                 Console.WriteLine("---------------------------------------------");
                 break;
         }
+        if (ValidOp == true) 
+        {
+
+            Console.WriteLine($"{num1} {op} {num2} = {result}");
+            Console.WriteLine("---------------------------------------------");
+            if (result % 2 == 0 && result>0)
+            {
+                Console.WriteLine($"{result} is even and positive");
+                Console.WriteLine("---------------------------------------------");
+            }
+            else if (result % 2 == 1 && result > 0)
+            {
+                Console.WriteLine($"{result} is odd and positive");
+                Console.WriteLine("---------------------------------------------");
+            }
+            else if (result % 2 == 0 && result < 0)
+            {
+                Console.WriteLine($"{result} is even and negative");
+                Console.WriteLine("---------------------------------------------");
+            }
+            else if (result % 2 == 1 && result < 0)
+            {
+                Console.WriteLine($"{result} is odd and negative");
+                Console.WriteLine("---------------------------------------------");
+            }
+            else
+            {
+                Console.WriteLine($"{result} is Zero");
+                Console.WriteLine("---------------------------------------------");
+            }
+
+        }
+
 
 
     }
